a9f072ab229dc626e6253ebb92d659aeba8e6e1f
Samuel Henrique Lima da Silva <samuelhenrique442@gmail.com>
2025-06-12 11:18:17 -0300

    modificaçoes

--- Transacoes_Bancarias/script.sql
+++ Transacoes_Bancarias/script.sql
@@ -14,5 +14,23 @@ FROM base_transacoes_banco
 WHERE STATUS = 'Efetivada'
 GROUP BY ClienteID, Nome;
 
+
+-- TESTE ANTES DO UPDATE PARA VER AS CIDADES
+SELECT * 
+FROM base_transacoes_banco
+WHERE TipoTransacao = 'TED'
+  AND Valor > 40000;
+
+
+-- QUERY EM SI
+BEGIN TRAN
+UPDATE base_transacoes_banco
+SET Cidade = 'Uberlandia'
+WHERE TipoTransacao = 'TED'
+AND Valor > 40000
+
+ROLLBACK; -- VOLTA
+COMMIT;   -- EFETIVA
+
 SELECT *
 FROM vw_total_por_cliente;
